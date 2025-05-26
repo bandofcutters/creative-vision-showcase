4c49b5beeea4207c36e3f80d0002c28d6d4b0c55
bandofcutters <121744307+bandofcutters@users.noreply.github.com>
2025-05-26 16:10:17 -0400

    Update TrueCrime.tsx
    Added useState

--- src/pages/TrueCrime.tsx
+++ src/pages/TrueCrime.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Play } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PortfolioNav } from "@/components/PortfolioNav";
 import { VideoModal } from "@/components/VideoModal";
 
